a528555ae2d270f1e223170601ece8fd7384e165
Andrew Balzer <andrewdbalzer@gmail.com>
2019-01-16 20:43:51 -0600

    refactoring around redux form for edit and create stream

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,12 +1,18 @@
+import { pick } from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { fetchStream } from '../../actions';
+import StreamForm from './StreamForm';
+import { fetchStream, editStream } from '../../actions';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.stream.id, formValues);
+  };
 
   render() {
     if (!this.props.stream) {
@@ -14,7 +20,13 @@ class StreamEdit extends React.Component {
     }
 
     return (
-      <div>{ this.props.stream.title }</div>
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
     );
   }
 }
@@ -23,4 +35,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
